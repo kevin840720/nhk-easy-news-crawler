5c46250e98ae6bc923118ca10f009b45f53659d8
Kevin.Wang <kevin.wang@gosaico.com>
2024-12-05 23:17:52 +0800

    [test]: add unittest for NHK News Parser

--- src/parser.py
+++ src/parser.py
@@ -6,6 +6,9 @@
 @Desc    :  None
 """
 
+from typing import Optional
+import json
+
 from bs4 import BeautifulSoup
 
 class NHKEasyNewsWebParser:
@@ -14,27 +17,96 @@ class NHKEasyNewsWebParser:
                  ) -> None:
         self.soup = soup
 
-    def article_html(self):
-        return self.soup.find("article", {"class": "easy-article"})
-
     @property
-    def title(self):
-        article_soup = self.article_html()
-        title_block = article_soup.find("h1", {"class": "article-title"})
+    def title(self) -> str:
+        title_block = (self.soup.find("h1", {"class": "article-title"})
+                       or self.soup.find("h1", {"class": "article-main__title"})  # 2024/04/01 之前
+                       )
         for rt_tag in title_block.find_all("rt"):
             rt_tag.decompose()
         return title_block.text
 
     @property
-    def date(self):
-        article_soup = self.article_html()
-        date_block = article_soup.find("p", {"class": "article-date"})
+    def date(self) -> str:
+        date_block = (self.soup.find("p", {"class": "article-date"})
+                      or self.soup.find("p", {"class": "article-main__date"})  # 2024/04/01 之前
+                      )
         return date_block.text
 
     @property
-    def body(self):
-        article_soup = self.article_html()
-        body_block = article_soup.find("div", {"class": "article-body"})
+    def body(self) -> str:
+        body_block = (self.soup.find("div", {"class": "article-body"})
+                      or self.soup.find("div", {"class": "article-main__body article-body"})  # 2024/04/01 之前
+                      )
         for rt_tag in body_block.find_all("rt"):
             rt_tag.decompose()
+        return body_block.text
+
+class NHKNewsWebParser:
+    def __init__(self,
+                 soup:BeautifulSoup,
+                 ) -> None:
+        self.soup = soup
+
+    def _get_article_meta(self) -> Optional[dict]:
+        scripts = self.soup.find_all('script', type='application/ld+json')
+        # 過濾只包含 "@type": "NewsArticle" 的 JSON
+        for script in scripts:
+            try:
+                data = json.loads(script.string)  # 將 JSON 字串解析為 Python 字典
+                if data.get("@type") == "NewsArticle":  # 檢查 @type 是否為 NewsArticle
+                    return data
+            except json.JSONDecodeError:
+                continue
+
+    @property
+    def title(self):
+        meta_data = self._get_article_meta()
+        if meta_data:
+            return meta_data["headline"]
+        # 如果抓不到 meta data ，則直接從文章中爬取
+        title_block = self.soup.find("h1", {"class": "content--title"})
+        return title_block.text
+
+    @property
+    def genre(self):
+        meta_data = self._get_article_meta()
+        if meta_data:
+            return meta_data["genre"]
+        return []
+
+    @property
+    def keywords(self):
+        meta_data = self._get_article_meta()
+        if meta_data:
+            return meta_data["keywords"]
+        return []
+
+    @property
+    def published_date(self):
+        meta_data = self._get_article_meta()
+        if meta_data:
+            return meta_data["datePublished"]
+        return None
+
+    @property
+    def modified_date(self):
+        meta_data = self._get_article_meta()
+        if meta_data:
+            return meta_data["dateModified"]
+        return None
+
+    @property
+    def date(self):
+        date_block = self.soup.find("p", {"class": "content--date"})
+        return date_block.find("time").text
+
+    @property
+    def summary(self):
+        body_block = self.soup.find("p", {"class": "content--summary"})
+        return body_block.text
+
+    @property
+    def body(self):
+        body_block = self.soup.find("div", {"class": "content--detail-more"})
         return body_block.text
